f8ed8bf1335602994bdde5f4d44cf165871c1214
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-27 10:17:15 +0200

    Commons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*
 1)Replica della grafica (immagine in allegato) con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse;
 Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto, ricavandoli dall'array contacts qui allegato;
-
+*//*
 2) Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all'interno del pannello della conversazione;
 Click sul contatto mostra la conversazione del contatto cliccato;
 
